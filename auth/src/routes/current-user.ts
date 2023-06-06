c2d550b9006bcf4cb3fdbf41c3b267219dea38c0
Kai <duykim.freedom@gmail.com>
2023-06-07 01:00:46 +0700

    change something

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send({ user: 'user' });
+  res.send({ user: 'use' });
 });
 
 export { router as currentUserRouter };
