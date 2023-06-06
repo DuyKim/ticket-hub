766f1b0b53963783b4866f955ceeb599c46bc080
Kai <duykim.freedom@gmail.com>
2023-06-07 03:26:31 +0700

    sdfdf

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send({ user: 'username' });
+  res.send({ user: 'finally, I create a full stack okteto' });
 });
 
 export { router as currentUserRouter };
