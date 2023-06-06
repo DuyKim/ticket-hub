170fe92bd4ab53fe3548667fd4f9f290e942b99d
Kai <duykim.freedom@gmail.com>
2023-06-07 01:30:38 +0700

    sadf

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send({ user: 'user' });
+  res.send({ user: 'username' });
 });
 
 export { router as currentUserRouter };
